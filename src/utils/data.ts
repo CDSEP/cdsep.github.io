7b04c097b2c57813c88ea104228696b5455822a5
Josué Garza <josuegarza.dev@gmail.com>
2023-12-23 12:40:02 -0600

    servicios

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,40 +1,55 @@
 const services = [
     {
-        title: "Social media marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        title: "Terapia Individual",
+        description: "Sesiones personalizadas para promover el crecimiento personal y manejar desafíos específicos.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 h-6">
+        <!-- A simple blue circle -->
+        <circle cx="12" cy="12" r="10" fill="#007bff"/>
+        </svg>`,
+    },
+
+    {
+        title: "Terapia de Parejas",
+        description: "Facilitamos herramientas para fortalecer las relaciones y mejorar la comunicación entre parejas.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 h-6">
+        <!-- Blue circle -->
+        <circle cx="12" cy="12" r="10" fill="#28a745"/>
+      </svg>
+      `,
+    },
+    {
+        title: "Terapia Familiar",
+        description: "Trabajamos con familias para resolver conflictos y mejorar la dinámica familiar.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 h-6">
+        <!-- A simple blue circle -->
+        <circle cx="12" cy="12" r="10" fill="#dc3545"/>
         </svg>`,
     },
     {
-        title: "Amazon affiliate marketing",
-        description: "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        title: "Mindfulness y Meditación",
+        description: "Servicios para mejorar la atención plena y reducir el estrés a través de la meditación.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 h-6">
+        <!-- A simple blue circle -->
+        <circle cx="12" cy="12" r="10" fill="#ffc107"/>
         </svg>`,
     },
     {
-        title: "Email marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        title: "Asesoramiento en Manejo del Estrés",
+        description: "Técnicas y estrategias para gestionar el estrés cotidiano de manera efectiva.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 h-6">
+        <!-- A simple blue circle -->
+        <circle cx="12" cy="12" r="10" fill="#6f42c1"/>
         </svg>`,
     },
     {
-        title: "Social media marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        title: "Desarrollo Personal",
+        description: "Programas diseñados para fomentar el crecimiento y desarrollo personal a largo plazo.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 h-6">
+        <!-- A simple blue circle -->
+        <circle cx="12" cy="12" r="10" fill="#20c997"/>
         </svg>`,
     },
-    {
-        title: "Amazon affiliate marketing",
-        description: "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
-    {
-        title: "Email marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
+
 ];
 
 export { services };
